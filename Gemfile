e0249b8e4e1cc39afade3d9b0a4d49831684a398
marcpt <cardosomarioar@gmail.com>
2015-02-26 19:31:22 +0000

    Added authorization

--- Gemfile
+++ Gemfile
@@ -50,3 +50,5 @@ gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootst
 gem 'simple_form'
 
 gem 'devise'
+
+gem 'cancan'
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,7 @@ class BooksController < ApplicationController
 
   # GET /books/1/edit
   def edit
+    authorize! :manage, @book
   end
 
   # POST /books
@@ -41,6 +42,8 @@ class BooksController < ApplicationController
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
   def update
+    authorize! :manage, @book
+
     respond_to do |format|
       if @book.update(book_params)
         format.html { redirect_to @book, notice: 'Book was successfully updated.' }
@@ -55,6 +58,8 @@ class BooksController < ApplicationController
   # DELETE /books/1
   # DELETE /books/1.json
   def destroy
+    authorize! :manage, @book
+    
     @book.destroy
     respond_to do |format|
       format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
